25ab915f3b01ca78ba49c39dec9081ff28888669
muschellij2 <muschellij2@gmail.com>
2017-10-30 12:49:30 -0400

    updated shiny app

--- build.R
+++ build.R
@@ -27,29 +27,31 @@ for(i in seq_along(rmds)){
   if(needs_making(rmds[i], htmls[i])){
     if(dirname(rmds[i]) == "."){
       render(rmds[i], output_format = html_document(toc = TRUE, toc_float = list(collapsed = FALSE),
-                                                    fig_caption = FALSE, theme = 'cosmo', toc_depth = 2), 
+                                                    fig_caption = FALSE, theme = 'cosmo', toc_depth = 2),
              output_dir = dirname(rmds[i]))
     } else {
-      render(rmds[i], output_format = ioslides_presentation(fig_height = 4.5, 
-                                                            fig_caption = FALSE, 
-                                                            logo = '../bloomberg_shield.png', 
-                                                            css = '../slides.css'), 
-             output_dir = dirname(rmds[i]))
+      render(rmds[i], output_format = ioslides_presentation(
+        fig_height = 6,
+        fig_caption = FALSE,
+        logo = '../bloomberg_shield.png',
+        css = '../slides.css',
+        widescreen = TRUE),
+        output_dir = dirname(rmds[i]))
     }
   } else {
     print(rmds[i])
     print(htmls[i])
   }
-  
+
   if(needs_making(rmds[i], pdfs[i]) && make_pdf){
     if(dirname(rmds[i]) == "."){
-      render(rmds[i], output_format = pdf_document(fig_caption = FALSE), 
+      render(rmds[i], output_format = pdf_document(fig_caption = FALSE),
              output_dir = dirname(rmds[i]))
     } else {
-      render(rmds[i], output_format = beamer_presentation(fig_caption = FALSE), 
+      render(rmds[i], output_format = beamer_presentation(fig_caption = FALSE),
              output_dir = dirname(rmds[i]))
     }
-    
+
   } else {
     print(rmds[i])
     print(pdfs[i])
